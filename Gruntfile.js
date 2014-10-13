5ec6f2561187cba42a92888023ec288435d4006d
0u812 <0u812@github.com>
2014-10-13 13:39:07 -0700

    fix syntax

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt) {
-  require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
+  require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
     shell: {
@@ -11,15 +11,13 @@ module.exports = function(grunt) {
       target: {
           command: 'emconfigure cmake -DCMAKE_TOOLCHAIN_FILE=<%= toolchain %> -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=<%= installpath %> <%= sourceroot %> && make -j8 && make install'
       }
-    }
+    },
 
-    toolchain: grunt.option('toolchain');
-    buildpath: grunt.option('build-path');
-    installpath: grunt.option('install-path');
-    sourceroot: grunt.option('source-root'); // good job losers https://github.com/gruntjs/grunt/issues/965
+    toolchain: grunt.option('toolchain'),
+    buildpath: grunt.option('build-path'),
+    installpath: grunt.option('install-path'),
+    sourceroot: grunt.option('source-root'),
   });
-//   console.log('sourceroot:\n');
-//   console.log(sourceroot);
 
   grunt.registerTask('default', ['shell']);
 };
