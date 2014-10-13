133f21903d59f55e8f6c01062c92267bd5d358d1
0u812 <0u812@github.com>
2014-10-13 13:44:20 -0700

    opt num jobs

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,13 +9,14 @@ module.exports = function(grunt) {
         }
       },
       target: {
-          command: 'emconfigure cmake -DCMAKE_TOOLCHAIN_FILE=<%= toolchain %> -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=<%= installpath %> <%= sourceroot %> && make -j8 && make install'
+          command: 'emconfigure cmake -DCMAKE_TOOLCHAIN_FILE=<%= toolchain %> -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=<%= installpath %> <%= sourceroot %> && make -j<%= jobs %> && make install'
       }
     },
 
     toolchain: grunt.option('toolchain'),
     buildpath: grunt.option('build-path') || './build',
     installpath: grunt.option('install-path') || './install',
+    jobs: grunt.option('jobs') || '8',
     sourceroot: __dirname,
   });
 
