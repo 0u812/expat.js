d6ade2ccb82b1f69d8c39a50fca364821c8e41ea
0u812 <0u812@github.com>
2014-10-13 13:37:48 -0700

    move tpl to obj

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,14 +12,14 @@ module.exports = function(grunt) {
           command: 'emconfigure cmake -DCMAKE_TOOLCHAIN_FILE=<%= toolchain %> -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=<%= installpath %> <%= sourceroot %> && make -j8 && make install'
       }
     }
-  });
 
-  var toolchain = grunt.option('toolchain');
-  var buildpath = grunt.option('build-path');
-  var installpath = grunt.option('install-path');
-  var sourceroot = grunt.option('source-root'); // good job losers https://github.com/gruntjs/grunt/issues/965
-  console.log('sourceroot:\n');
-  console.log(sourceroot);
+    toolchain: grunt.option('toolchain');
+    buildpath: grunt.option('build-path');
+    installpath: grunt.option('install-path');
+    sourceroot: grunt.option('source-root'); // good job losers https://github.com/gruntjs/grunt/issues/965
+  });
+//   console.log('sourceroot:\n');
+//   console.log(sourceroot);
 
   grunt.registerTask('default', ['shell']);
 };
